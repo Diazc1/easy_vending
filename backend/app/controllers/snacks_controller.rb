a7679de751faaec548bf88390d7509214bb24edb
Cathy Diaz <Cathydiaz14@gmail.com>
2021-05-28 12:47:49 -0700

    made all instance variables normal variables because I don't need instance variables anymore. Instance variables are used in the views files and I don't need views files for this project

--- backend/app/controllers/snacks_controller.rb
+++ backend/app/controllers/snacks_controller.rb
@@ -3,45 +3,45 @@ class SnacksController < ApplicationController
 
   # GET /snacks
   def index
-    @snacks = Snack.all
+    snacks = Snack.all
 
-    render json: @snacks
+    render json: snacks
   end
 
   # GET /snacks/1
   def show
-    render json: @snack
+    render json: snack
   end
 
   # POST /snacks
   def create
-    @snack = Snack.new(snack_params)
+    snack = Snack.new(snack_params)
 
-    if @snack.save
-      render json: @snack, status: :created, location: @snack
+    if snack.save
+      render json: snack, status: :created, location: snack
     else
-      render json: @snack.errors, status: :unprocessable_entity
+      render json: snack.errors, status: :unprocessable_entity
     end
   end
 
   # PATCH/PUT /snacks/1
   def update
-    if @snack.update(snack_params)
-      render json: @snack
+    if snack.update(snack_params)
+      render json: snack
     else
-      render json: @snack.errors, status: :unprocessable_entity
+      render json: snack.errors, status: :unprocessable_entity
     end
   end
 
   # DELETE /snacks/1
   def destroy
-    @snack.destroy
+    snack.destroy
   end
 
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_snack
-      @snack = Snack.find(params[:id])
+      snack = Snack.find(params[:id])
     end
 
     # Only allow a list of trusted parameters through.
