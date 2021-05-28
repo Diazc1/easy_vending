0060b0f2cbd10786d9eb9845d7bd35bd8285ae76
Cathy Diaz <Cathydiaz14@gmail.com>
2021-05-28 12:46:12 -0700

    made all instance variables normal variables because I don't need instance variables anymore. Instance variables are used in the views files and I don't need views files for this project

--- backend/app/controllers/vending_machines_controller.rb
+++ backend/app/controllers/vending_machines_controller.rb
@@ -3,45 +3,45 @@ class VendingMachinesController < ApplicationController
 
   # GET /vending_machines
   def index
-    @vending_machines = VendingMachine.all
+    vending_machines = VendingMachine.all
 
-    render json: @vending_machines
+    render json: vending_machines
   end
 
   # GET /vending_machines/1
   def show
-    render json: @vending_machine
+    render json: vending_machine
   end
 
   # POST /vending_machines
   def create
-    @vending_machine = VendingMachine.new(vending_machine_params)
+    vending_machine = VendingMachine.new(vending_machine_params)
 
-    if @vending_machine.save
-      render json: @vending_machine, status: :created, location: @vending_machine
+    if vending_machine.save
+      render json: vending_machine, status: :created, location: vending_machine
     else
-      render json: @vending_machine.errors, status: :unprocessable_entity
+      render json: vending_machine.errors, status: :unprocessable_entity
     end
   end
 
   # PATCH/PUT /vending_machines/1
   def update
-    if @vending_machine.update(vending_machine_params)
-      render json: @vending_machine
+    if vending_machine.update(vending_machine_params)
+      render json: vending_machine
     else
-      render json: @vending_machine.errors, status: :unprocessable_entity
+      render json: vending_machine.errors, status: :unprocessable_entity
     end
   end
 
   # DELETE /vending_machines/1
   def destroy
-    @vending_machine.destroy
+    vending_machine.destroy
   end
 
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_vending_machine
-      @vending_machine = VendingMachine.find(params[:id])
+      vending_machine = VendingMachine.find(params[:id])
     end
 
     # Only allow a list of trusted parameters through.
