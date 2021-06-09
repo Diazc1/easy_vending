4ab27e22805dc26b04280fa83c5985a5b7ba9750
Cathy Diaz <Cathydiaz14@gmail.com>
2021-06-09 14:08:17 -0700

    create const variables for create form handler form, prevented default.

--- frontend/index.js
+++ frontend/index.js
@@ -2,6 +2,11 @@ const endPoint = "http://localhost:3000/api/v1/snacks"
 
 document.addEventListener('DOMContentLoaded', () => {
     getSnacks()
+
+    const createSnackForm = document.querySelector("#create-snack-form")
+
+    createSnackForm.addEventListener("submit", (e) => createFormHandler(e))
+
 })
 
 function getSnacks() {
@@ -13,7 +18,7 @@ function getSnacks() {
 
             const snackMarkup = `
             <div data-id=${snack.id}>
-            <h3>${snack.attributes.vending_machine.location}</h3>
+            <h3>${snack.attributes.vending_machine.location} (${snack.attributes.vending_machine.id})</h3>
             Snack Name: <p>${snack.attributes.name}</p>
             Category: <p>${snack.attributes.category}</p>
             Beginning Inventory: <p>${snack.attributes.beginning_inventory}</p>
@@ -29,3 +34,25 @@ function getSnacks() {
 
     })
 }
+
+
+function createFormHandler(e) {
+    e.preventDefault()
+    const nameInput = document.querySelector("#input-name").value
+    const categoryInput = document.querySelector("#input-category").value
+    const beginning_inventoryInput = document.querySelector("#input-beginning_inventory").value
+    const ending_inventoryInput = document.querySelector("#input-ending_inventory").value
+    const priceInput = document.querySelector("#input-price").value
+    const profitInput = document.querySelector("#input-profit").value
+    const vending_machineID = parseInt(document.querySelector("#vending_machines").value)
+
+    postFetch(nameInput, categoryInput, beginning_inventoryInput, ending_inventoryInput, priceInput, profitInput, vending_machineID)
+}
+
+function postFetch(name, category, beginning_inventory, ending_inventory, price, profit, vending_machine_id) {
+    console.log(name, category, beginning_inventory, ending_inventory, price, profit, vending_machine_id)
+
+}
+
+
+
