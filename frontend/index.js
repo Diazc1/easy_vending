91777934af0439f9d6c15f4c0c6cb9eb97ddfd3f
Cathy Diaz <Cathydiaz14@gmail.com>
2021-06-09 19:00:47 -0700

    create POST fetch request/ set a const variable equal to snack.data.attributes to make code more DRY.

--- frontend/index.js
+++ frontend/index.js
@@ -44,14 +44,42 @@ function createFormHandler(e) {
     const ending_inventoryInput = document.querySelector("#input-ending_inventory").value
     const priceInput = document.querySelector("#input-price").value
     const profitInput = document.querySelector("#input-profit").value
-    const vending_machineID = parseInt(document.querySelector("#vending_machines").value)
+    const vending_machineId = parseInt(document.querySelector("#vending_machines").value)
 
-    postFetch(nameInput, categoryInput, beginning_inventoryInput, ending_inventoryInput, priceInput, profitInput, vending_machineID)
+    postFetch(nameInput, categoryInput, beginning_inventoryInput, ending_inventoryInput, priceInput, profitInput, vending_machineId)
 }
 
 function postFetch(name, category, beginning_inventory, ending_inventory, price, profit, vending_machine_id) {
-    console.log(name, category, beginning_inventory, ending_inventory, price, profit, vending_machine_id)
+    
+    const bodyData = {name, category, beginning_inventory, ending_inventory, price, profit, vending_machine_id}
+    
+    fetch(endPoint, {
+        // POST request
+        method: "POST",
+        headers: {"Content-Type": "application/json"},
+        body: JSON.stringify(bodyData)
+    })
+    .then(response => response.json())
+    .then(snack => {
+        const snackData = snack.data.attributes
+        // // render JSON response
+        const snackMarkup = `
+        <div data-id=${snack.id}>
 
+        <h3>${snackData.vending_machine.location} (${snackData.vending_machine.id})</h3>
+
+        Snack Name: <p>${snackData.name}</p>
+        Category: <p>${snackData.category}</p>
+        Beginning Inventory: <p>${snackData.beginning_inventory}</p>
+        Ending Inventory: <p>${snackData.ending_inventory}</p>
+        Price: <p>${snackData.price}</p>
+        Profit: <p>${snackData.profit}</p>
+        <button data-id=${snack.id}>edit</button>
+        </div>
+        <br><br>`;
+
+        document.querySelector('#snack-container').innerHTML += snackMarkup
+    }) 
 }
 
 
