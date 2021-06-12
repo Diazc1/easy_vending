9704a4891cb7f4376b9301d6350605730c2243b3
Cathy Diaz <Cathydiaz14@gmail.com>
2021-06-11 19:29:01 -0700

    added validations

--- backend/app/models/snack.rb
+++ backend/app/models/snack.rb
@@ -1,3 +1,5 @@
 class Snack < ApplicationRecord
   belongs_to :vending_machine
+
+  validates :name, :category, :beginning_inventory, :ending_inventory, :price, :profit, presence: true
 end
--- backend/app/models/vending_machine.rb
+++ backend/app/models/vending_machine.rb
@@ -1,3 +1,4 @@
 class VendingMachine < ApplicationRecord
     has_many :snacks
+    validates :location, presence: true 
 end
