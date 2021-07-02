3e4d8274e8b321176db645afa9f2d4a264056ec1
Cathy Diaz <Cathydiaz14@gmail.com>
2021-07-02 02:58:38 -0700

    debugged

--- backend/app/controllers/api/v1/snacks_controller.rb
+++ backend/app/controllers/api/v1/snacks_controller.rb
@@ -16,12 +16,12 @@ class Api::V1::SnacksController < ApplicationController
 
     # POST /snacks
     def create
-        snack = Snack.new(snack_params) 
+        snack = Snack.new(snack_params)
 
         if snack.save
             render json: SnackSerializer.new(snack), status: :accepted
         else
-            render json: {errors: snack.errors.full_messages}, status: :unprocessible_entity
+            render json: {errors: snack.errors.full_messages}, status: :unprocessable_entity
         end
     end
 
