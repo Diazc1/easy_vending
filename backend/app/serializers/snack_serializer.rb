7b81f7f0c41df61a56c6920560de85cb426fe869
Cathy Diaz <Cathydiaz14@gmail.com>
2021-07-02 02:57:32 -0700

    ...

--- backend/app/serializers/snack_serializer.rb
+++ backend/app/serializers/snack_serializer.rb
@@ -1,6 +1,6 @@
 class SnackSerializer
   
   include FastJsonapi::ObjectSerializer
-  attributes :name, :category, :beginning_inventory, :ending_inventory, :price, :profit, :vending_machine
+  attributes :name, :category, :beginning_inventory, :ending_inventory, :price, :profit, :vending_machine_id, :vending_machine
 
 end
