376627f621f691d0b4092bcad261f2d9d6f48afb
Cathy Diaz <Cathydiaz14@gmail.com>
2021-07-21 15:47:30 -0700

    create Snack class and a constructor that pushes all instances of 'this' into an empty array

--- frontend/src/snack.js
+++ frontend/src/snack.js
@@ -1,1 +1,17 @@
-console.log("in snack js")
+class Snack {
+
+    constructor(snack, snackAttributes) {
+        this.id = snack.id
+        this.name = snackAttributes.name
+        this.category = snackAttributes.category
+        this.beginning_iventory = snackAttributes.beginning_iventory
+        this.ending_inventory = snackAttributes.ending_inventory
+        this.price = snackAttributes.price
+        this.profit = snackAttributes.profit
+
+        Snack.all.push(this)
+    }
+}
+
+
+Snack.all = [];
